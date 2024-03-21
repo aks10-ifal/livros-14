316520f5dcd48a680f7f941911b8c4636c809c62
aks10-ifal <140767449+aks10-ifal@users.noreply.github.com>
2024-03-20 21:56:04 -0300

    Update app.js

--- app.js
+++ app.js
@@ -15,5 +15,5 @@ app.get('/', (req, res) => {
   res.redirect('/livros');
 });
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Servidor Rodando na Porta: ${port}`))
